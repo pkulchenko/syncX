dea8feccb9be60f5290cb68cc31bbfb0ef2a9204
Paul Kulchenko <paul@kulchenko.com>
2021-03-11 22:42:37 -0800

    Added more complex tests for deletes.

--- test.lua
+++ test.lua
@@ -2,6 +2,14 @@ require "testwell"
 
 local sync9 = require "sync9"
 
+-- testing get/set methods
+dag = sync9.createnode("0", {'X', '1', '2', '3'})
+is(dag:getvalue(), "X123", "Initial node created.")
+dag:set(0, "0")
+is(dag:getvalue(), "0123", "Set processed.")
+is(dag:get(0), "0", "Get processed (1/2).")
+is(dag:get(3), "3", "Get processed (2/2).")
+
 -- testing inserts
 local dag = sync9.createnode("0", {'X', '1'})
 is(dag:getvalue(), "X1", "Initial node created.")
@@ -17,17 +25,9 @@ is(dag:getvalue(), "XABCDCBA1", "Patch processed with no deletes and no addition
 -- testing deletes
 dag = sync9.createnode("0", {'X', '1', '2', '3'})
 is(dag:getvalue(), "X123", "Initial node created.")
-dag:addpatchset("20", {{1, 2, {'A'}}, {2, 0, {'B'}}})
-is(dag:getvalue(), "XAB3", "Patchset processed with two patches with elements added and deleted.")
-dag:addpatchset("30", {{2, 2, {}}})
-is(dag:getvalue(), "XA", "Patch processed with 2 elements deleted.")
+dag:addpatchset("20", {{1, 2, {'A'}}, {2, 0, {'B', 'B'}}, {3, 1, {'C', 'D'}}})
+is(dag:getvalue(), "XABCD3", "Patchset processed with three patches with elements added and deleted.")
+dag:addpatchset("30", {{1, 4, {}}})
+is(dag:getvalue(), "X3", "Patch processed with 2 elements deleted.")
 dag:addpatchset("40", {{0, 2, {'C', 'C'}}})
 is(dag:getvalue(), "CC", "Patch processed with 2 elements deleted and 2 added at position 0.")
-
--- testing get/set methods
-dag = sync9.createnode("0", {'X', '1', '2', '3'})
-is(dag:getvalue(), "X123", "Initial node created.")
-dag:set(0, "0")
-is(dag:getvalue(), "0123", "Set processed.")
-is(dag:get(0), "0", "Get processed (1/2).")
-is(dag:get(3), "3", "Get processed (2/2).")
